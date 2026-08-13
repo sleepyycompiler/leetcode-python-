ade0bef3b137c5a8bf81f304e808c53b96ec6a3e
sleepyycompiler <generalusage269@gmail.com>
2026-08-13 20:58:15 +0530

    Time: 54 ms (95.53%), Space: 19.3 MB (87.82%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        results=[]
+        for i in range(n):
+            results.append(nums[i])
+            results.append(nums[i+n])
+        return results
